fe4f54f6f31faae6df4b84383a68f700c3ba4251
Piyush kumar <iampiyushkr@gmail.com>
2021-09-27 17:20:39 +0530

    making uploads

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -4,8 +4,17 @@ const express=require("express");
 const User=require("../model/user.model")
 const upload=require("../middlewares/file_upload")
 const router=express.Router()
+router.get("",async (req,res)=>{
+    const user=await User.find().lean().exec();
 
-router.post("/single",upload.single("productImages"),async (req,res)=>{
+    return res.status(201).json({user});
+})
+router.post("",async (req,res)=>{
+    const user=await User.create(req.body);
+
+    return res.status(201).json({user});
+})
+router.post("/single",upload.single("photo_url"),async (req,res)=>{
     const user=await User.create({
         name:req.body.name,
         email:req.body.email,
@@ -14,6 +23,7 @@ router.post("/single",upload.single("productImages"),async (req,res)=>{
         photo_url:req.file.path
 
     });
+
     return res.status(201).json({user});
 })
 
--- src/middlewares/file_upload.js
+++ src/middlewares/file_upload.js
@@ -2,18 +2,21 @@ const multer=require("multer");
 const path=require("path");
 const storage=multer.diskStorage({
     destination:function(req,file,callback){
-        callback(null,path.join(_dirname,"../uploads"));
+        callback(null,"uploads/");
     },
     filename:function(req,file,callback){
-        const uniqueSuffix=Date.now()+"-"+Math.round(Math.random()*1E9)
-        cb(null,file.fieldname+"-"+uniqueSuffix+file.originalname)
+        // const uniqueSuffix=Date.now()+"-"+Math.round(Math.random()*1E9)
+        // callback(null,file.fieldname+"-"+uniqueSuffix+file.originalname)
+        let ext=path.extname(file.originalname)
+        callback(null,Date.now()+ext)
+
     }
 })
 
-const fileFilter=(req,file,cb)=>{
-    if(file.mimetype==="image/jpeg"||
-    file.mimetype==="image/png"||
-    file.mimetype==="image/jpg"
+const fileFilter=(req,file,callback)=>{
+    if(file.mimetype=="image/jpeg"||
+    file.mimetype=="image/png"||
+    file.mimetype=="image/jpg"
     
     ){
         callback(null,true);
@@ -26,7 +29,7 @@ const fileFilter=(req,file,cb)=>{
 const upload=multer({
     storage:storage,
     limits:{
-        fileSize:1024*1024*500,
+        fileSize:1024*1024*5,
         files:5
     },
     fileFilter:fileFilter
